89fac2ea888cc91e6c89040f339e34c5ac805f26
dustinkincaid <Dustin.Kincaid@uvm.edu>
2020-10-30 11:32:49 -0400

    * Updated SOM script for hford * Updated parameters used for hford SOM

--- Scripts/runSOM_hford.R
+++ Scripts/runSOM_hford.R
@@ -1,20 +1,27 @@
 # Run self-organizing map to cluster event metrics
-# This script is based of of the R script, sedRegData_V2.R, from Kristen Underwood
+# This script is based off of the R script, sedRegData_V2.R, from Kristen Underwood
+# Edited according to Brittany L's Run_SOM.R script, which is an adaptation (more generalized?) version of KU's
 
 # Load packages ----
   library("tidyverse") # general workhorse
   library("kohonen")   # to run the SOM
   library("vegan")     # to run adonis() for calc of nonparametric F test
+
+  # Load the range normalization function written by Kristin Underwood
+  source("Scripts/L2norm.R")
+
+# Create a new folder to save the plots and CSVs produced by the SOM code
+newFolder <- paste("Data/somResults/", Sys.Date(), sep = "")
+dir.create(newFolder)
   
 # Read in data ----
   # Calculated event metrics for each site as calculated in compile_calculate_allVars.R
   hford <- read_csv("Data/eventMetrics_hford.csv")
 
-
 # HUNGERFORD ----  
   # PREPARE DATA ----
     # To begin, let's only use observations where there are no NAs
-    dat_hf <- hford %>% 
+    myData <- hford %>% 
       # Remove non-numerical columns
       select(-c(site, season, event_start)) %>% 
       # Remove response variables
@@ -22,31 +29,38 @@
       # Keep the multipeak column for now, but code it as numerical
       mutate(multipeak = ifelse(multipeak == "NO", 0, 1)) %>% 
       # Keep only complete observations/rows (no NAs in any of the columns)
-      na.omit()
-  # We're left with only 44 obs/rows of 76; but if we determine certain variables aren't useful for clustering
-  # we can remove them and reiterate this process and may end up with more obs/rows
+      na.omit() %>% 
+      # Add a obs/row number
+      mutate(obs = row_number())
+    # We're left with only 44 obs/rows of 76; but if we determine certain variables aren't useful for clustering
+    # we can remove them and reiterate this process and may end up with more obs/rows
+  
+    # Not exactly sure why this exists; relic of KU's script
+    myDataSet <- c("hford")  #identify data set version
+    
+    # Vector of observation number from original dataset
+    observ <- dplyr::pull(myData, obs)
   
   # SET UP THE GRID/LATTICE FOR THE SOM ----
+  # Run this code once to determine which dimensions to input into the params_SOM . . .csv file that is saved as the 'params' object below
     # According to the heuristic rule from Vesanto 2000, number of grid elements/grid size/nodes = 5 * sqrt(n)
-    VesantoNodes_hf = round(5 * sqrt(nrow(dat_hf)), 0)
+    VesantoNodes = round(5 * sqrt(nrow(myData)), 0)
     
     # To determine the the shape of the grid, we'll determine the ratio of columns to rows using the ratio of 
     # the first two eigen values of the input data set as recommended by Park et al. 2006
     # PCA
     # scale = TRUE = correlation matrix; scale = FALSE = covariance matrix
-    PCA_hf <- prcomp(dat_hf, scale = TRUE, center = TRUE)
-    eigenValues_hf <- PCA_hf$sdev^2
-    first_hf <- nth(eigenValues_hf, 1)
-    second_hf <- nth(eigenValues_hf, 2)
-    gridRatio_hf <- round((first_hf/second_hf), 1)
+    PCA <- prcomp(myData, scale = TRUE, center = TRUE)
+    eigenValues <- PCA$sdev^2
+    first <- nth(eigenValues, 1)
+    second <- nth(eigenValues, 2)
+    gridRatio <- round((first/second), 1)
   
     # Input this information into the params_SOM . . .csv file
-  
-  
-
     
     
 # SOM Code from Kristen Underwood's sedRegData_V2.R script
+# Also adapted by referencing Brittney L's Run_SOM.R script
 # ------------- Define Color Palettes of SOM plots  -------------------------------------------
 
 # Colour palette definition - this added from blog comments (found in 2014-01 CSO_SOM.R script)
@@ -67,7 +81,7 @@ sedRegPallette=c("cyan", "yellow", "orange", "red", "green3", "orchid","gray", "
 
 # Read in table of parameters (grid rows, cols, grid type, learning rate etc)
 ### IMPORTANT: cannot have number nodes greater than num observations
-params <- read.csv("params_SOM.csv",na.strings=".", header=T)
+params <- read_csv("Data/params_SOM_hford.csv")
 params <- as.matrix(params[1:nrow(params),])
 
 # initialize matrix to store results of each SOM run
@@ -103,13 +117,14 @@ for (i in 1:nrow(params)) {
       
       if (normMeth == "scale") {
             # Method 1 - z transformation (subtract mean, divide by stdev)
-            myData.sc <- scale(myData_num)  #scale is a base R function
+            myData.sc <- scale(myData)  #scale is a base R function
             
       } else {
             # Method 2 - L2 normalization by variable (scale between 0 and 1)
             # normMeth <- c("L2")
-            source("L2norm.R")  #function has been created by KU
-            myData.sc <- L2norm(myData_num)
+            # I load this up top (note from DK)
+            # source("L2norm.R")  #function has been created by KU
+            myData.sc <- L2norm(myData)
       }
       
       # ----------------- Weight the inputs (Optional) ---------------------------------
@@ -159,6 +174,10 @@ for (i in 1:nrow(params)) {
       code.dists <- object.distances(som_model, type = "codes")
       
       un.dists <- unit.distances(myGrid)
+      
+      check1 <- 'check1'
+      print(check1)
+      
       # ------------------- Assign Nodes to a Cluster -------------------------------------
       
       #according to dissimilarity between trained weight vectors
@@ -169,10 +188,11 @@ for (i in 1:nrow(params)) {
       #NEW CODE
       som_cluster <- cutree(hclust(code.dists), nclusters)
      
-    
+      check2 <- 'check2'
+      print(check2)    
       # -------------- Visualize results in various plots (exported to pdf) --------------------------
-      myFileName2 <- paste(myRun,"SOMplots_LkChamp",myDataSet,wghtMeth,normMeth, paste0(rows,"x", cols, myTopo), paste0(nclusters,"cl"), sep = "_")
-      pdf(file=myFileName2, height=8, width=10)  #dev.off() is at 400
+      myFileName2 <- paste(myRun,"SOMplots",myDataSet,wghtMeth,normMeth, paste0(rows,"x", cols, myTopo), paste0(nclusters,"cl"), sep = "_")
+      pdf(file=paste(newFolder, myFileName2, sep = "/"), height=8, width=10)  #dev.off() is at 400
       par(mfrow=c(1,2), mar= c(5.5, 4, 4, 2) + 0.1)
       opar<-par()
       
@@ -190,6 +210,9 @@ for (i in 1:nrow(params)) {
       mtext(paste(myFileName2), side=3, outer=TRUE)
       # A good mapping should show small distances everywhere in the map". (Wehrens & Buydens, 2007)
       # mean distance of objects, mapped to a particular node, to the codebook vector of that node
+      
+      check3 <- 'check3'
+      print(check3)
       
       # Training Map
       plot(som_model, type="changes", main = "Training Progress")
@@ -218,6 +241,10 @@ for (i in 1:nrow(params)) {
       # U-Matrix with cluster boundaries
       plot(som_model, type = "dist.neighbours", main = "U Matrix", shape = "straight", palette.name=grey.colors)
       add.cluster.boundaries(som_model, som_cluster)
+      
+      check4 <- 'check4'
+      print(check4)
+      
       
       # Component Planes (Heat Maps) - Standardized Variable
       # Plot the component planes for each variable using a loop:
@@ -257,11 +284,15 @@ for (i in 1:nrow(params)) {
       #plot(som_model, type = "mapping", labels = som_cluster, col=som_cluster+1, main="Mapping Plot")
       #add.cluster.boundaries(som_model, som_cluster)
       
+      check5 <- 'check5'
+      print(check5)      
+      
+      # Brittany didn't use the following chunck; commenting out for now
       ### Now match node numbers back to original [nrow] input patterns for plotting
-      newlabels <- cbind(som_model$unit.classif, observ, mySRClass)
-      plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], cex=0.75, main="Input Patterns \nMapped to each Node")
-      #plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], col = sedRegPallette[as.integer(mySRClass)], cex=0.75, main="Input Patterns \nMapped to each Node")
-      add.cluster.boundaries(som_model, som_cluster)
+      # newlabels <- cbind(som_model$unit.classif, observ, mySRClass)
+      # plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], cex=0.75, main="Input Patterns \nMapped to each Node")
+      # #plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], col = sedRegPallette[as.integer(mySRClass)], cex=0.75, main="Input Patterns \nMapped to each Node")
+      # add.cluster.boundaries(som_model, som_cluster)
       
       ### Now match the cluster assignments back to the original [nrow] input patterns for plotting:
       # such as the line graphs with color coding for clusters that Peter Isles made
@@ -287,7 +318,8 @@ for (i in 1:nrow(params)) {
             }
             
             dataMeans<-apply(myData.sc, 2, mean)  #compute overall mean for comparison
-            barplot(clusterMeans-dataMeans, names.arg=names(clusterData), las=3, main=paste("Cluster ", p, sep=""), col=barPalette[p])
+            barplot(clusterMeans-dataMeans, names.arg=names(clusterData), ylim = c(-.8,.8), las=3, main=paste("Cluster ", p, sep=""), col=barPalette[p])
+            # barplot(clusterMeans-dataMeans, names.arg=names(clusterData), las=3, main=paste("Cluster ", p, sep=""), col=barPalette[p])
             mtext(paste("n =", numPatterns), side = 3, line = -0.5)
       }  #end for p loop  #ylim = c(-1.2, 1.2),
       
@@ -315,14 +347,15 @@ for (i in 1:nrow(params)) {
       add.cluster.boundaries(som_model, som_cluster) 
       #mtext(paste("group mean - data mean for vars", normMeth), side=3, outer=TRUE)
       
+      # Brittany did not use the next 2 chunks; commenting out for now
       #repeat plot of input patterns mapped to each node for ref next to cluster plot
-      plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], cex=0.75, main="Input Patterns \nMapped to each Node")
-      add.cluster.boundaries(som_model, som_cluster)
+      # plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], cex=0.75, main="Input Patterns \nMapped to each Node")
+      # add.cluster.boundaries(som_model, som_cluster)
       
       #repeat large plot of input patterns mapped to each node for ref next to cluster plot
-      par(mfrow=c(1,1), mar=c(6,3,3,1), oma=c(0,0,3,0))
-      plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], col = sedRegPallette[as.integer(mySRClass)], cex=1.1, main="Input Patterns \nMapped to each Node \nColored by Mike Class")
-      add.cluster.boundaries(som_model, som_cluster)
+      # par(mfrow=c(1,1), mar=c(6,3,3,1), oma=c(0,0,3,0))
+      # plot(som_model, type = "mapping", shape = "straight", labels = newlabels[ ,2], col = sedRegPallette[as.integer(mySRClass)], cex=1.1, main="Input Patterns \nMapped to each Node \nColored by Mike Class")
+      # add.cluster.boundaries(som_model, som_cluster)
       
       
       
@@ -338,6 +371,9 @@ for (i in 1:nrow(params)) {
       #extract F ratio and pVal for storage in results table
       npF <- npFtest$F[1]
       pVal_npF <- npFtest$`Pr(>F)`[1]
+      
+      check6 <- 'check6'
+      print(check6)
       
       # pp <- 1 #use this line to break loop for debugging
       # #for (pp in 1:length(unique(cluster_assign))) {
@@ -366,16 +402,18 @@ for (i in 1:nrow(params)) {
       Results[i,17] <- round(pVal_npF, digits = 6)
       
       clustAssignments <- rbind(clustAssignments, cluster_assign)
+        
       
 } # end for loop of SOM runs
 
 # ----------- Format and Export Results Tables  ------------------------------------
+# Create a new folder for results
 colnames(Results) <- c('Run','DataSet','Nodes','Topol','rows','cols','normMeth', 
                        'wghtMeth','niter', 'alphaCrs', 'alphaFin', 
                        'Clusters', 'ColRowRat', 'Dist_mn','QE','npF','pVal_npF','blank') 
 
 ResultsDF <- as.data.frame(Results)
-write.csv(ResultsDF, paste0("Results_", myDataSet,"_",nclusters,"_cl",".csv"))
+write_csv(ResultsDF, paste0(newFolder,"/","Results_", myDataSet,"_",nclusters,"_cl",".csv"))
 
 clustAssignm <- clustAssignments[-1, ]
 colnames(clustAssignm) <- as.character(paste0("obs",observ))
@@ -384,5 +422,7 @@ colnames(clustAssignm) <- as.character(paste0("obs",observ))
 clustAssignm <- cbind (Results, clustAssignm)
 
 clustAssignmDF <- as.data.frame(clustAssignm)
-write.csv(clustAssignmDF, paste0("ClustAssign_",myDataSet,"_",nclusters,"_cl",".csv"))    
-  
+write_csv(clustAssignmDF, paste0(newFolder,"/","ClustAssign_",myDataSet,"_",nclusters,"_cl",".csv"))
+
+finished <- 'finished'
+print(finished)
